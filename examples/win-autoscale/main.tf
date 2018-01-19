1a28a54e72e4e7f157663c3817b813d7887a5682
Long C Lam <31355535+eemperor@users.noreply.github.com>
2018-01-19 14:33:56 -0500

    Simplify Travis CI test and updated tf files spacing

--- examples/win-autoscale/main.tf
+++ examples/win-autoscale/main.tf
@@ -7,12 +7,12 @@ provider "aws" {
 module "win-autoscale" {
   source = "git::https://github.com/plus3it/terraform-aws-watchmaker//modules/win-autoscale/"
 
-  Name                   ="test123winautostack"
-  AmiId                  = "ami-0a792a70"
-  AppVolumeSize          = "10"
-  KeyPairName            = "test123key"
-  InstanceRole           = "test123role"
-  NoPublicIp             = "false"
-  SecurityGroupIds       = "sg-12345678"
-  SubnetIds               = "subnet-12345678,subnet-87654321"
+  Name             = "test123winautostack"
+  AmiId            = "ami-0a792a70"
+  AppVolumeSize    = "10"
+  KeyPairName      = "test123key"
+  InstanceRole     = "test123role"
+  NoPublicIp       = "false"
+  SecurityGroupIds = "sg-12345678"
+  SubnetIds        = "subnet-12345678,subnet-87654321"
 }
--- examples/win-instance/main.tf
+++ examples/win-instance/main.tf
@@ -7,13 +7,13 @@ provider "aws" {
 module "win-instance" {
   source = "git::https://github.com/plus3it/terraform-aws-watchmaker//modules/win-instance/"
 
-  Name                   ="test123winstack"
-  AmiId                  = "ami-0a792a70"
-  AppVolumeSize          = "10"
-  KeyPairName            = "test123key"
-  InstanceRole           = "test123role"
-  PrivateIp              = ""
-  NoPublicIp             = "false"
-  SecurityGroupIds       = "sg-12345678"
-  SubnetId               = "subnet-87654321"
+  Name             = "test123winstack"
+  AmiId            = "ami-0a792a70"
+  AppVolumeSize    = "10"
+  KeyPairName      = "test123key"
+  InstanceRole     = "test123role"
+  PrivateIp        = ""
+  NoPublicIp       = "false"
+  SecurityGroupIds = "sg-12345678"
+  SubnetId         = "subnet-87654321"
 }
