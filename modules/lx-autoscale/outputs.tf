a1101d4073414fe9f1d462b2513fe6ca6806ab82
userhas404d <29389186+userhas404d@users.noreply.github.com>
2018-05-07 11:36:45 -0400

    breakout cfn outputs

--- modules/lx-autoscale/outputs.tf
+++ modules/lx-autoscale/outputs.tf
@@ -1,11 +1,31 @@
 #Outputs from the Linux Autoscaling Terraform module.
 
 output "watchmaker-lx-autoscale-stack-id" {
-  description = "This output provides the CloudFormation stack id of the Watchmaker Linux instance"
+  description = "CloudFormation stack id"
   value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.id}"
 }
 
-output "watchmaker-lx-autoscale-stack-outputs" {
-  description = "This output provides the outputs of the CloudFormation stack"
-  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs}"
+output "watchmaker-lx-autoscale-scale-down-scheduled-action" {
+  description = "Scale Down Scheduled Action ID"
+  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs["ScaleDownScheduledAction"]}"
+}
+
+output "watchmaker-lx-autoscale-scale-up-scheduled-action" {
+  description = "Scale Up Scheduled Action ID"
+  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs["ScaleUpScheduledAction"]}"
+}
+
+output "watchmaker-lx-autoscale-autoscaling-group-id" {
+  description = "Autoscaling Group ID"
+  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs["WatchmakerAutoScalingGroupId"]}"
+}
+
+output "watchmaker-lx-autoscale-launch-config-id" {
+  description = "Launch Configuration ID"
+  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs["WatchmakerLaunchConfigId"]}"
+}
+
+output "watchmaker-lx-autoscale-launch-config-log-group-name" {
+  description = "Log Group Name"
+  value       = "${aws_cloudformation_stack.watchmaker-lx-autoscale.outputs["WatchmakerLaunchConfigLogGroupName"]}"
 }
--- modules/win-autoscale/outputs.tf
+++ modules/win-autoscale/outputs.tf
@@ -1,11 +1,31 @@
 #Outputs from the Windows Autoscaling Terraform module.
 
 output "watchmaker-win-autoscale-stack-id" {
-  description = "This output provides the CloudFormation stack id"
+  description = "CloudFormation stack id"
   value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.id}"
 }
 
-output "watchmaker-win-autoscale-stack-outputs" {
-  description = "This output provides the outputs of the CloudFormation stack"
-  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs}"
+output "watchmaker-win-autoscale-scale-down-scheduled-action" {
+  description = "Scale Down Scheduled Action ID"
+  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs["ScaleDownScheduledAction"]}"
+}
+
+output "watchmaker-win-autoscale-scale-up-scheduled-action" {
+  description = "Scale Up Scheduled Action ID"
+  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs["ScaleUpScheduledAction"]}"
+}
+
+output "watchmaker-win-autoscale-autoscaling-group-id" {
+  description = "Autoscaling Group ID"
+  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs["WatchmakerAutoScalingGroupId"]}"
+}
+
+output "watchmaker-win-autoscale-launch-config-id" {
+  description = "Launch Configuration ID"
+  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs["WatchmakerLaunchConfigId"]}"
+}
+
+output "watchmaker-win-autoscale-launch-config-log-group-name" {
+  description = "Log Group Name"
+  value       = "${aws_cloudformation_stack.watchmaker-win-autoscale.outputs["WatchmakerLaunchConfigLogGroupName"]}"
 }
